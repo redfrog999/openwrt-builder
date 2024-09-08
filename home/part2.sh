2337a99567291634b8e567c5363dbe45cd4c722e
David Tai <redfrog999@users.noreply.github.com>
2024-09-08 20:16:05 +0800

    Update part2.sh

--- home/part2.sh
+++ home/part2.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
 
-# Replace the official source package
-./scripts/feeds install -p vergilgao golang xray-core v2ray-geodata sing-box
+# Modify default IP 
+sed -i 's/192.168.1.1/192.168.68.1/g' package/base-files/files/bin/config_generate
 
-# replace default theme to argon
-rm -rf feeds/luci/themes/luci-theme-design/
-sed -i 's/luci-theme-bootstrap/luci-theme-design/' feeds/luci/collections/luci*/Makefile
+# 最大连接数修改为65535
+sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package/base-files/files/etc/sysctl.conf
 
-# Modify DHCP client for iptv
-sed -i 's/\${vendorid:+-V "$vendorid"}/-V \"\"/g' package/network/config/netifd/files/lib/netifd/proto/dhcp.sh
+echo '替换golang到1.22.x'
+rm -rf feeds/packages/lang/golang
+git clone -b 22.x --single-branch https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
+echo '=========Replace golang OK!========='
