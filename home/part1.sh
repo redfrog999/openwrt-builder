99c843742089299cbbc280fd0da971aa1ac2f313
David Tai <redfrog999@users.noreply.github.com>
2024-09-08 20:14:50 +0800

    Update part1.sh

--- home/part1.sh
+++ home/part1.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-sed -i '1i src-git vergilgao https://github.com/VergilGao/openwrt-packages.git;openwrt-23.05' feeds.conf.default
+sed -i '1i src-git redfrog999 https://github.com/redfrog999/openwrt.git;openwrt-23.05' feeds.conf.default
 cp -rf ../config/diff/* .
+
+# Add a feed source
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;luci-smartdns-dev" >> "feeds.conf.default"
+#echo "src-git turboacc https://github.com/chenmozhijin/turboacc;luci" >> "feeds.conf.default"
+
+# 添加插件源码
+sed -i '$a src-git-full kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
