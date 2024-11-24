d41ee052acbc05e8811325a83b76e08178e65dbb
David Tai <redfrog999@users.noreply.github.com>
2024-11-24 20:54:28 +0800

    Update part1.sh

--- home/part1.sh
+++ home/part1.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#sed -i '1i src-git redfrog999 https://github.com/redfrog999/immortalwrt.git;openwrt-21.02' feeds.conf.default
-#cp -rf ../config/diff/* .
+sed -i '1i src-git redfrog999 https://github.com/redfrog999/packages.git;openwrt-21.02' feeds.conf.default
+cp -rf ../config/diff/* .
 
 # Add a feed source
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
-echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
+echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
 #echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >> "feeds.conf.default"
 
 # 添加插件源码
