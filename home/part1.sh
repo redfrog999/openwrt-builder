f2a034cf030678d964eca2d1467f346a5f1ceaae
David Tai <redfrog999@users.noreply.github.com>
2025-02-16 09:24:48 +0800

    Update part1.sh

--- home/part1.sh
+++ home/part1.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-sed -i '1i src-git redfrog999 https://github.com/redfrog999/packages.git;openwrt-23.05' feeds.conf.default
+sed -i '1i src-git redfrog999 https://github.com/redfrog999/packages-lede.git;master' feeds.conf.default
 cp -rf ../config/diff/* .
 
 # Add a feed source
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
-echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >> "feeds.conf.default"
+echo "src-git nkki https://github.com/nikkinikki-org/OpenWrt-nikki" >> "feeds.conf.default"
 
 # 添加插件源码
 #sed -i '$a src-git kiddin9 https://github.com/redfrog999/kiddin9-openwrt-packages' feeds.conf.default
 sed -i '$a src-git-full kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i 's/+firewall/+uci-firewall/g' feeds/luci/applications/luci-app-firewall/Makefile
+#sed -i 's/+firewall/+uci-firewall/g' feeds/luci/applications/luci-app-firewall/Makefile
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
